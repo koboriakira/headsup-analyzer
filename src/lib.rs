6e438580b79c55424c16a2604cf74a8f5deb67fd
Akira Kobori <private.beats@gmail.com>
2021-03-01 22:58:56 +0900

    Implement prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,118 @@
-pub mod handrange {
+pub mod player {
+    use crate::{
+        action::Action,
+        cards::{self, Cards},
+        position::Position,
+    };
+    use anyhow::{bail, Context, Result};
+    use rust_poker::{hand_evaluator::Hand, hand_range::HandRange};
     use serde::{Deserialize, Serialize};
+    use std::{collections::HashMap, fs::File, io::BufReader, str::FromStr};
+
+    #[derive(Debug)]
+    pub struct Player {
+        cards: Cards,
+        pub hand_range: HandRange,
+        action: Action,
+        position: Position,
+    }
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Patterns {
-        patterns: Vec<HashMap<String, String>>,
+        patterns: Vec<Pattern>,
     }
 
-    use std::{collections::HashMap, fs::File, io::BufReader};
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Pattern {
+        position: String,
+        villain: String,
+        action: String,
+        hands: String,
+    }
 
-    pub fn load_json() {
-        let result = File::open("range.json")
-            .and_then(|file| Ok(BufReader::new(file)))
-            .and_then(|reader| {
-                let deserialized: Patterns = serde_json::from_reader(reader).unwrap();
-                Ok(deserialized)
-            });
-        match result {
-            Ok(patterns) => patterns
-                .patterns
-                .into_iter()
-                .for_each(|p| println!("{:?}", p)),
-            Err(any) => {
-                // dbg!(any);
-                println!("{:#?}", any);
+    impl Player {
+        pub fn new(
+            cards: Cards,
+            hero_position: Position,
+            villain_positon: Position,
+            villain_action: Action,
+        ) -> Result<Self> {
+            let (hero_position, villain_positon, hero_action) =
+                collect_position_and_action(hero_position, villain_positon, villain_action);
+            get_hand_range(&hero_position, &villain_positon, &hero_action).and_then(|hand_range| {
+                Ok(Self {
+                    cards,
+                    hand_range,
+                    action: hero_action,
+                    position: hero_position,
+                })
+            })
+        }
+    }
+
+    fn collect_position_and_action(
+        hero_position: Position,
+        villain_position: Position,
+        villain_action: Action,
+    ) -> (Position, Position, Action) {
+        let hero_action = villain_action.to_hero_action();
+        if hero_action.is_after_three_bet() {
+            let hero_position = hero_position.convert_to_ip_or_oop(villain_position);
+            let villain_position = hero_position.invert();
+            return (hero_position, villain_position, hero_action);
+        }
+        if hero_action.eq(&Action::Open) {
+            return (hero_position, Position::NONE, hero_action);
+        }
+        (hero_position, villain_position, hero_action)
+    }
+
+    fn get_hand_range(
+        hero_position: &Position,
+        villain_positon: &Position,
+        hero_action: &Action,
+    ) -> Result<HandRange> {
+        let json = load_json()?;
+        let pattern = json.patterns.into_iter().find(|p| {
+            if !Position::from_str(&p.position).unwrap().eq(hero_position) {
+                return false;
             }
-        };
+            if !Position::from_str(&p.villain).unwrap().eq(villain_positon) {
+                return false;
+            }
+            if !Action::from_str(&p.action).unwrap().eq(hero_action) {
+                return false;
+            }
+            true
+        });
+        match pattern {
+            Some(pattern) => Ok(HandRange::from_string(pattern.hands)),
+            None => Err(anyhow::anyhow!(format!(
+                "can't find HandRange. {:?} vs. {:?} : {:?}",
+                hero_position, villain_positon, hero_action
+            ))),
+        }
+    }
+
+    fn load_json() -> Result<Patterns> {
+        let file = File::open("range.json").context("can't load json file".to_string())?;
+        let reader = BufReader::new(file);
+        let deserialized: Patterns =
+            serde_json::from_reader(reader).with_context(|| "can't read json file".to_string())?;
+        Ok(deserialized)
     }
 }
 
 pub mod position {
     use std::str::FromStr;
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
     pub enum Position {
+        SB,
+        BB,
         UTG,
         MP,
         CO,
         BTN,
-        SB,
-        BB,
         OOP,
         IP,
         NONE,
@@ -61,6 +136,24 @@ pub mod position {
         }
     }
 
+    impl Position {
+        pub fn convert_to_ip_or_oop(&self, villain_positon: Position) -> Position {
+            if self > &villain_positon {
+                Position::OOP
+            } else {
+                Position::IP
+            }
+        }
+
+        pub fn invert(&self) -> Self {
+            match self {
+                Position::IP => Position::OOP,
+                Position::OOP => Position::IP,
+                other => other.clone(),
+            }
+        }
+    }
+
     #[cfg(test)]
     mod tests {
         use std::str::FromStr;
@@ -76,7 +169,7 @@ pub mod position {
 
 pub mod action {
     use std::str::FromStr;
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
     pub enum Action {
         Open,
         Limp,
@@ -94,11 +187,32 @@ pub mod action {
                 "OPEN" => Ok(Self::Open),
                 "LIMP" => Ok(Self::Limp),
                 "CALL" => Ok(Self::Call),
-                "THREEBET" => Ok(Self::ThreeBet),
-                "THREEBETCALL" => Ok(Self::ThreeBetCall),
-                "FOURBET" => Ok(Self::FourBet),
-                "FOURBETCALL" => Ok(Self::FourBetCall),
+                "THREEBET" | "3BET" => Ok(Self::ThreeBet),
+                "THREEBETCALL" | "3BETCALL" => Ok(Self::ThreeBetCall),
+                "FOURBET" | "4BET" => Ok(Self::FourBet),
+                "FOURBETCALL" | "4BETCALL" => Ok(Self::FourBetCall),
                 _ => return Err(format!("Invalid action: {}", s)),
+            }
+        }
+    }
+
+    impl Action {
+        pub fn to_hero_action(&self) -> Self {
+            match self {
+                Self::Open => Self::Call,
+                Self::Limp => Self::Limp,
+                Self::Call => Self::Open,
+                Self::ThreeBet => Self::ThreeBetCall,
+                Self::ThreeBetCall => Self::ThreeBet,
+                Self::FourBet => Self::FourBetCall,
+                Self::FourBetCall => Self::FourBet,
+            }
+        }
+
+        pub fn is_after_three_bet(&self) -> bool {
+            match self {
+                Self::ThreeBetCall | Self::FourBet | Self::FourBetCall => true,
+                _ => false,
             }
         }
     }
@@ -109,6 +223,12 @@ pub mod cards {
     use std::str::FromStr;
     #[derive(Debug)]
     pub struct Cards(u64);
+
+    impl Cards {
+        pub fn new(value: u64) -> Self {
+            Self(value)
+        }
+    }
 
     impl FromStr for Cards {
         type Err = String;
@@ -133,24 +253,25 @@ pub mod argparse {
     )]
     pub struct Opts {
         #[clap(name = "YOUR_POSITION")]
-        hero_position: Position,
+        pub hero_position: Position,
         #[clap(name = "YOUR_CARDS")]
-        hero_cards: Cards,
+        pub cards: Cards,
         #[clap(name = "VILLAIN_POSITION")]
-        villain_positon: Position,
+        pub villain_positon: Position,
         #[clap(name = "VILLAIN_ACTION")]
-        villain_action: Action,
+        pub villain_action: Action,
         #[clap(name = "BOARD")]
-        board: Cards,
+        pub board: String,
     }
 
-    pub fn arg_parse() {
+    pub fn arg_parse() -> Opts {
         let opts = Opts::parse();
-        println!("{:?}", opts.hero_position);
-        println!("{:?}", opts.hero_cards);
-        println!("{:?}", opts.villain_positon);
-        println!("{:?}", opts.villain_action);
-        println!("{:?}", opts.board);
+        // println!("{:?}", opts.hero_position);
+        // println!("{:?}", opts.cards);
+        // println!("{:?}", opts.villain_positon);
+        // println!("{:?}", opts.villain_action);
+        // println!("{:?}", opts.board);
+        opts
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-use headsup_analyzer::{argparse::arg_parse, handrange::load_json};
+use std::fmt::Debug;
+
+use headsup_analyzer::{argparse::arg_parse, cards::Cards, player::Player};
+use rust_poker::hand_range::get_card_mask;
 fn main() {
-    arg_parse();
-    load_json();
+    let opts = arg_parse();
+    let hero = Player::new(
+        opts.cards,
+        opts.hero_position.clone(),
+        opts.villain_positon.clone(),
+        opts.villain_action.clone(),
+    )
+    .unwrap();
+    let villain = Player::new(
+        Cards::new(0),
+        opts.villain_positon,
+        opts.hero_position,
+        opts.villain_action.to_hero_action(),
+    )
+    .unwrap();
+    println!("{}", &hero.hand_range.hands);
+    let test_combo = &hero.hand_range.hands.first().unwrap();
+    println!("{}", test_combo);
+    println!("{:?}", test_combo);
+    // println!("{:?}", &villain);
+    let public_cards = get_card_mask(&opts.board);
+    let equities = rust_poker::equity_calculator::calc_equity(
+        &[hero.hand_range, villain.hand_range].to_vec(),
+        public_cards,
+        4,
+        1000,
+    );
+    println!("Win Rate: {:?}", (equities[0] * 100.0).round() / 100.0);
 }
